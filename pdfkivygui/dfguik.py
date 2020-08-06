2a86f96ed091f693abb59ebda8154c64876180b3
pythonpanda <jefpadfi@gmail.com>
2020-08-05 23:03:11 -0600

    Edited backend_kivy.py for github to be able to build it.

--- pdfkivygui/dfguik.py
+++ pdfkivygui/dfguik.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import kivy
 
 kivy.require('1.11.0')
@@ -595,3 +597,34 @@ class DfguiWidget(TabbedPanel):
         # print(str(arr))
         self.panel1.apply_filter(self.panel3.get_filters())
         self.panel1._generate_table(disabled=self.panel2.get_disabled_columns())
+
+
+def create_dummy_data(size):
+
+    user_ids = np.random.randint(1, 1000000, 10)
+    product_ids = np.random.randint(1, 1000000, 100)
+
+    def choice(*values):
+        return np.random.choice(values, size)
+
+    random_dates = [
+        datetime.date(2016, 1, 1) + datetime.timedelta(days=int(delta))
+        for delta in np.random.randint(1, 50, size)
+    ]
+    return pd.DataFrame.from_dict({
+        "Date": random_dates,
+        "UserID": choice(*user_ids),
+        "ProductID": choice(*product_ids),
+        "IntColumn": choice(1, 2, 3),
+        "FloatColumn": choice(np.nan, 1.0, 2.0, 3.0),
+        "StringColumn": choice("A", "B", "C"),
+        "Gaussian 1": np.random.normal(0, 1, size),
+        "Gaussian 2": np.random.normal(0, 1, size),
+        "Uniform": np.random.uniform(0, 1, size),
+        "Binomial": np.random.binomial(20, 0.1, size),
+        "Poisson": np.random.poisson(1.0, size),
+    })
+
+
+if __name__ == "__main__":
+    dfg = DfguiWidget(1000)
